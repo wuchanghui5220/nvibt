d92cacc4efe5f88049b225eab69578fa59d3ee1e
Vincent <33740652+wuchanghui5220@users.noreply.github.com>
2023-12-11 09:26:07 +0800

    Update ibeasylinkinfo.sh

--- ibeasylinkinfo.sh
+++ ibeasylinkinfo.sh
@@ -1,1 +1,10 @@
-ibnetdiscover  -p |grep -v ??? |grep -v "Quantum Mellanox Technologies" |grep ^SW |grep -v "Mellanox Technologies Aggregation Node"  |grep -v "Mellanox" |awk '{printf "%-4s %-3s %-10s  <-->  %-4s %-3s %-20s\n", $2, $13, $3, $10, $15, $9}'
+#!/bin/bash
+
+ibnetdiscover -p | sed 's/ HCA/_HCA/g' \
+  | grep -v ??? \
+  | grep -v "Quantum Mellanox Technologies" \
+  | grep ^SW \
+  | grep -v "Mellanox Technologies Aggregation Node" \
+  | grep -v "Mellanox" \
+  | awk '{printf "%-3s %-s %-3s  <-->  %-3s %-3s %-3s\n", "lid-"$2, $13, "["$3"]", "["$10"]", $15, "lid-"$9}' \
+  | sed 's/_HCA-/ HCA-/g'
