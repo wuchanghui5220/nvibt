3a34016e4ac18ce2b408915474e304140cfcffc9
Vincent <33740652+wuchanghui5220@users.noreply.github.com>
2023-12-12 13:10:57 +0800

    Update ibcablesinfo.sh

--- ibcablesinfo.sh
+++ ibcablesinfo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mst restart &>/dev/null
+mst cable add --with_ib  &>/dev/null
+
 cables=$(mst status | awk '/Cables:/ {found=1; next} found {print $1}' |grep -v "^-")
 
 echo "Wavelength,Vendor,Serial number,Part number,Temperature [c],Length [m]"
